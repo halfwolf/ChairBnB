54cf0757ef5f36df8369243ed85d26e6d94ad944
halfwolf <c.day108@gmail.com>
2014-11-20 22:58:59 -0500

    add welcome to home page and links to popular places

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -5,15 +5,15 @@ ChairBnB.Routers.Router = Backbone.Router.extend({
     this.listings.fetch(),
     this.bindSearchEvents()
   },
-  
+
   events: {
-    
+
   },
-  
-  
+
+
   routes: {
     'listings': 'listingsIndex',
-    'search/:query': 'mapView', 
+    'search/:query': 'mapView',
     'listings/new': 'listingNew',
     'listings/:id': 'listingShow',
     'users/:id': 'userShow',
@@ -24,15 +24,10 @@ ChairBnB.Routers.Router = Backbone.Router.extend({
     'myreservations': 'myReservations',
     'myreviews': 'myReviews'
   },
-  
+
   bindSearchEvents: function(){
     var searchBox = $('input.search-form')
     var that = this
-    searchBox.on('click', function(){      
-      if (searchBox.val() === "Search...") {
-        searchBox.val("");
-      }
-    }) 
     searchBox.keydown(function(event) {
       if (event.keyCode === 13) {
         var query = encodeURIComponent(event.currentTarget.value)
@@ -41,15 +36,11 @@ ChairBnB.Routers.Router = Backbone.Router.extend({
     })
 
   },
-  
-  search: function() {
-    alert("we made it")
-  },
-  
+
   mapView: function(query) {
     var location = decodeURIComponent(query)
     var listings = new ChairBnB.Collections.Listings()
-    
+
     listings.fetch({ data:{min: "0,0", max: "0,0" }})
     var view = new ChairBnB.Views.Map({
       query: location,
@@ -58,7 +49,7 @@ ChairBnB.Routers.Router = Backbone.Router.extend({
     this._swapView(view)
 
   },
-  
+
   listingsIndex: function() {
     this.listings.fetch()
     var view = new ChairBnB.Views.ListingsIndex({
@@ -66,17 +57,17 @@ ChairBnB.Routers.Router = Backbone.Router.extend({
     })
     this._swapView(view)
   },
-  
+
   listingShow: function(id) {
     var chair = this.listings.getOrFetch(id);
-    
+
     var view = new ChairBnB.Views.ListingShow({
       model: chair
     })
-    
+
     this._swapView(view)
   },
-  
+
   listingNew: function() {
     var chair = new ChairBnB.Models.Listing()
     var view = new ChairBnB.Views.ListingForm({
@@ -85,82 +76,82 @@ ChairBnB.Routers.Router = Backbone.Router.extend({
     });
     this._swapView(view);
   },
-  
+
   userShow: function(id) {
-    
+
     var user = new ChairBnB.Collections.Users().getOrFetch(id)
-    
+
     var view = new ChairBnB.Views.UserShow({
       model: user
     })
-    
+
     this._swapView(view)
   },
-  
+
   dashboardShow: function() {
     ChairBnB.Models.me.fetch();
     var view = new ChairBnB.Views.DashboardShow({
       model: ChairBnB.Models.me
     });
-    
-    this._swapView(view);    
+
+    this._swapView(view);
   },
-  
+
   myListings: function() {
     ChairBnB.Models.me.listings().fetch();
     var view = new ChairBnB.Views.MyListings({
-      collection: ChairBnB.Models.me.listings() 
+      collection: ChairBnB.Models.me.listings()
     })
-    
+
     this._swapView(view);
   },
-  
+
   conversationsIndex: function() {
     ChairBnB.Models.me.conversations().fetch();
-    
+
     var view = new ChairBnB.Views.ConversationsIndex({
       collection: ChairBnB.Models.me.conversations()
     })
-    
+
     this._swapView(view)
   },
-  
+
   conversationShow: function(id) {
     var conversation = ChairBnB.Models.me.conversations().getOrFetch(id);
-    
+
     var view = new ChairBnB.Views.ConversationShow({
       model: conversation
     })
-    
+
     this._swapView(view);
   },
-  
+
   myReservations: function() {
     ChairBnB.Models.me.reservations().fetch();
-    
+
     var view = new ChairBnB.Views.MyReservations({
       collection: ChairBnB.Models.me.reservations()
     })
-    
+
     this._swapView(view);
-    
+
   },
-  
+
   myReviews: function() {
     ChairBnB.Models.me.reviews().fetch();
-    
+
     var view = new ChairBnB.Views.MyReviews({
       collection: ChairBnB.Models.me.reviews()
     })
-    
+
     this._swapView(view);
   },
-  
-  
+
+
   _swapView: function(view) {
     this._currentView && this._currentView.remove();
     this.currentView = view;
     this.$rootEl.html(view.render().$el);
   }
-  
+
 })
